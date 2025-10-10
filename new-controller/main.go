29d486fdfc8e2edb9671426da92af7d3b54f6d98
Vladyslav Prokopenko <13607594+roko99@users.noreply.github.com>
2025-10-10 09:34:54 +0300

    feat: add controller (#7)

--- new-controller/main.go
+++ new-controller/main.go
@@ -2,18 +2,23 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"net/http"
 
+	// k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
-	newv1 "github.com/mastering-k8s/new-controller/api/v1alpha1"
-	"github.com/mastering-k8s/new-controller/controllers"
+	newv1 "github.com/roko99/mastering-k8s/new-controller/api/v1alpha1"
+	"github.com/roko99/mastering-k8s/new-controller/controllers"
 )
 
 func main() {
@@ -31,15 +36,44 @@ func main() {
 
 	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
 
+	// Create a variable to hold the client that will be set after manager creation
+	var k8sClient client.Client
+
+	// Create health check handler that uses the client
+	healthCheck := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.Background()
+		list := &newv1.NewResourceList{}
+		if err := k8sClient.List(ctx, list); err != nil {
+			// if k8serrors.IsNotFound(err) || meta.IsNoMatchError(err) {
+			if meta.IsNoMatchError(err) {
+				http.Error(w, "NewResource CRD not found", http.StatusServiceUnavailable)
+				return
+			}
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
 	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{
-		Scheme:           scheme,
-		Metrics:          server.Options{BindAddress: metricsAddr},
-		LeaderElection:   enableLeaderElection,
-		LeaderElectionID: "newresource-controller",
+		Scheme: scheme,
+		Metrics: server.Options{
+			BindAddress: metricsAddr,
+			ExtraHandlers: map[string]http.Handler{
+				"/healthz": healthCheck,
+			},
+		},
+		HealthProbeBindAddress: "", // Disable separate health probe server
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "newresource-controller",
 	})
 	if err != nil {
 		panic(err)
 	}
+
+	// Set the client after manager is created
+	k8sClient = mgr.GetClient()
 
 	if err := (&controllers.NewResourceReconciler{
 		Client: mgr.GetClient(),
@@ -47,6 +81,7 @@ func main() {
 		panic(err)
 	}
 
+	// Start the manager (this is blocking)
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		panic(err)
 	}
